25e739befed3463b485998f7a3741787672bac36
znss2 <158584472+znss2@users.noreply.github.com>
2024-11-12 06:07:43 +0000

    api키 입력

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-#import openai
+import openai
 
 st.title("gpt-4o-mini 체험")
 
-api_key = st.text_input("api키를 입력하세요", "api키", type="password")
+api_key = st.text_input("api키를 입력하세요", type="password")
