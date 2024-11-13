6670cfa996f2d9dd28556d10723aeea9f5a945e0
znss2 <158584472+znss2@users.noreply.github.com>
2024-11-13 06:26:07 +0000

    add session_state

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from openai import OpenAI
  
 
 api_key = st.text_input("OpenAI API Key",key="user_api_key", type='password')
-st.session_state.user_api_key
+st.session_state.user_api_key = api_key
 
 client = OpenAI(api_key=api_key)
 
