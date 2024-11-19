d105c566b2ed20fc7348f3c40f0db77f9582029e
znss2 <158584472+znss2@users.noreply.github.com>
2024-11-19 06:46:47 +0000

    added img generate with dall-e

--- pages/4_Chat.py
+++ pages/4_Chat.py
@@ -1,11 +1,19 @@
 import streamlit as st
-import openai
 
 client = st.session_state.get('openai_client', None)
 if client is None:
     if st.button("API Key를 입력하세요."):
         st.switch_page("pages/1_Setting.py")
     st.stop()
+
+def generate_img(prompt):
+    response = client.images.generate(
+        model="dall-e-3",
+        prompt=prompt,
+        n=1,
+        size="1024x1024"
+    )
+    return response.data[0].url
 
 st.header("My Chat")
 
@@ -14,6 +22,22 @@ assistant = client.beta.assistants.create(
   model="gpt-4o-mini",
   tools=[{"type": "code_interpreter"}]
 )
+
+tools = [
+    {
+        "type":"function",
+        "function": {
+            "name":"generate_img",
+            "description":"Dall-E를 이용해 이미지를 생성하고 이미지 파일 이름을 반환.",
+            "parameters": {
+                "type":"object",
+                "properties": {
+                    "prompt": {"type":"string", "description":"image generation prompt"}
+                }
+            }
+        }
+    }
+]
 
 thread = client.beta.threads.create()
 
